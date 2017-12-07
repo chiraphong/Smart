bd0940743adc4e3bb4c93c79f1eda1b0b7ac1113
chraphong <32881229+chiraphong@users.noreply.github.com>
2017-12-07 17:25:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,6 +32,12 @@ if (!is_null($events['events'])) {
 				'text' => $text
 				
 			];
+			if($text == 'A'){
+        $msg = '0';
+      }
+      if($text == 'B'){
+        $msg = '1';
+      }
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
