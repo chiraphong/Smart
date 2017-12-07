5c4bee4f9027da577b4be27f4a17f627b5cc8e75
chraphong <32881229+chiraphong@users.noreply.github.com>
2017-12-07 17:20:35 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -13,7 +13,7 @@
     if($pos){
       $splitMsg = explode(":", $lineMsg);
       $topic = $splitMsg[0];
-      $msg = $splitMsg[2];
+      $msg = $splitMsg[1];
       pubMqtt($topic,$msg);
     }else{
       $topic = $Topic;
