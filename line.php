180ba2e8c258d6ab407454c9e063c1b463ff400e
chraphong <32881229+chiraphong@users.noreply.github.com>
2017-12-07 17:02:48 +0700

    Update line.php

--- line.php
+++ line.php
@@ -10,8 +10,7 @@ function send_LINE($msg){
       ];
       if($text == "ON"){
         $msg = 0;
-      }
-      if($text == "OFF"){
+      }else if($text == "OFF"){
         $msg = 1;
       }
       // Make a POST Request to Messaging API to reply to sender
