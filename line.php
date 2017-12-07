c36d31246fe3c3b8efc49bf5247c201fc351d2fb
chraphong <32881229+chiraphong@users.noreply.github.com>
2017-12-07 09:25:55 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,8 +2,8 @@
   
 
 function send_LINE($msg){
- $access_token = 'WlBuMy2FZL9jwdXovCDHUDG3y7KXw5fZ4IdkLpXwMl+LuGeCKFAsOd1cTSKl8A6ItXM3EjMY6gqVi3FObND2OcblW87bC7DD4T0+i5hGUzoJ4YgrBxaz5AGdWi00AOBPGjkxLm6MBhFrZLF2lHlOtwdB04t89/1O/w1cDnyilFU='
-  $messages = [
+ $access_token = 'Hp2UOQhamsXUNEJexD/MFND5SYbRNA7Gxdg5wKf0eAgF58+irTNcEcsAUIvCws2htXM3EjMY6gqVi3FObND2OcblW87bC7DD4T0+i5hGUzpl1RzdrC8HbRI0ONGrFy4u0/l18hdpXPJQ3xckhA6jPgdB04t89/1O/w1cDnyilFU='
+ $messages = [
         'type' => 'text',
         'text' => $msg
         //'text' => $text
