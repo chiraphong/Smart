9bcb32bcaf690a33b9914756922ea4cba05feccd
chraphong <32881229+chiraphong@users.noreply.github.com>
2017-12-07 17:29:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,6 +2,7 @@
  require("pub.php");
  require("line.php");
 
+$access_token = 'IECWvICftD2m9F5X2UCtIKeWXYn5E6X6zxxlvrS/gP5msX7xnsCxqmF7yEO1BSyetXM3EjMY6gqVi3FObND2OcblW87bC7DD4T0+i5hGUzr1Qio45Pw/JAPie8Ycyvl5/QtBQ22/aq/3DPufaHWb0wdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -26,6 +27,28 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+		        $messages = [
+				'type' => 'text',
+				'text' => $text
+				
+			];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
 		
 			
 			$Topic = "NodeMCU1" ;
