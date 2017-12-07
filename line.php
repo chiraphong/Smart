71ba70f69f0927691ba424db5a2a9bd98eef3944
chraphong <32881229+chiraphong@users.noreply.github.com>
2017-12-07 17:24:37 +0700

    Update line.php

--- line.php
+++ line.php
@@ -8,12 +8,7 @@ function send_LINE($msg){
         'text' => $msg
         
       ];
-      if($text == 'A'){
-        $msg = 0;
-      }
-      if($text == 'B'){
-        $msg = 1;
-      }
+      
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
