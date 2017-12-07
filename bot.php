3fb09391634e619dd5e7cef634f0ee8e77d6c857
chraphong <32881229+chiraphong@users.noreply.github.com>
2017-12-08 03:04:37 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
       			];
 			
 			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			/*$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
@@ -50,7 +50,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-			echo $result . "\r\n";
+			echo $result . "\r\n";*/
 		
 		       
 			$Topic = "NodeMCU1" ;
